e67b64966689e815a5e27ad759e0151636bb52af
elcineyjr <elcineyjr@gmail.com>
2021-09-28 19:59:11 -0300

    update runall command

--- runall.sh
+++ runall.sh
@@ -10,14 +10,15 @@ BIN_PATH=$SOURCE_PATH/bin
 ANTLR_PATH=$ROOT/tools/antlr-4.9.2-complete.jar
 CLASS_PATH_OPTION="-cp .:$ANTLR_PATH"
 
+# code generation flag because the interpreter flag will stop execution in some files for input
+flag=-c
+
 rm -rf $OUT
-mkdir $OUT
+mkdir -p $OUT
 for infile in `ls $IN/*.go`; do
     base=$(basename $infile)
-    outfile=$OUT/${base/.go/.out}
-    dotfile=$OUT/${base/.go/.dot}
-    pdffile=$OUT/${base/.go/.pdf}
-    echo Running $base
-    java $CLASS_PATH_OPTION:$BIN_PATH checker/Main $infile 1> $outfile 2> $dotfile
-    dot -Tpdf $dotfile -o $pdffile
+    outfile=$OUT/${base/.go/.nstm}
+    echo
+    echo Compiling $base
+    java $CLASS_PATH_OPTION:$BIN_PATH Main $infile $flag > $outfile 
 done
